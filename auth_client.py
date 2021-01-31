b5b7232081b5b6228663951b0f8e88d1042b82b1
vinit <vinit.james24@gmail.com>
2021-02-01 00:28:42 +0100

    added response_auth and htttp functions api based functions are yet to be added

--- auth_client.py
+++ auth_client.py
@@ -1,6 +1,7 @@
 from public_client import PublicClient
 from api_def import AuthenticatedAPI
 from requests import Session
+from utils import create_query_string, generate_signature
 
 class AuthenticatedClient(PublicClient, AuthenticatedAPI):
 
@@ -32,8 +33,54 @@ class AuthenticatedClient(PublicClient, AuthenticatedAPI):
     def _create_futures_api_uri(self, path: str):
         return self.FUTURES_API__URL + '/' + self.FUTURES_API_VERSION + '/' + path
 
-    def _request_auth(self, ):
+    def _request_auth(self, method: str, path: str,
+                      verified: bool, forced_params = False, **params):
+        kwargs = {}
+        kwargs['timeout'] = 10
+        if self._requests_params:
+            kwargs.update(self.response_params)
+        
+        if verified:
+            params['timestamp'] = int(time.time() * 1000)
+            query_string = create_query_string(params)
+            params['signature'] = generate_signature(query_string = query_string,
+                                                     api_secret = self.api_secret)
+
+        kwargs[params]=params
+            
+        response = getattr(self.session, method)(uri, **kwargs)
+        return self.handle_response(response)
+
+    
+    def _get(self, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+                return self._request_api('get', path, signed, version, **kwargs)
+
+    def _post(self, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+        return self._request_api('post', path, signed, version, **kwargs)
+    
+    def _put(self, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+        return self._request_api('put', path, signed, version, **kwargs)
+
+    def _delete(self, path, signed=False, version=PUBLIC_API_VERSION, **kwargs):
+        return self._request_api('delete', path, signed, version, **kwargs)
+
+    def create(self,
+               symbol: str,
+               side: str,
+               type: str,
+               timestamp: int,
+               timeInForce: str = None,
+               quantity: int = None,
+               quoteOrderQty: int = None,
+               price: float = None,
+               newClientOrderId: str = None,
+               stopPrice: float = None,
+               icebergQty: float = None,
+               newOrderRespType: str =None,
+               recvWindow: int = None):
+
+    # to do add conditions on parameters
         pass
         
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace() 
+    pass
