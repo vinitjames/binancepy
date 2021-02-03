b54095affe74fc8b59a21dfb500e7cbc915b6584
vinit <vinit.james24@gmail.com>
2021-02-03 16:52:46 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+        name='binancepy',
+        version='0.0.1',
+        packages=['binance'],
+        description='Binance REST API python implementation',
+        url='https://github.com/vinitjames/binancepy',
+        author='Vinit James',
+        license='MIT',
+        author_email='vinit.james24@gmail.com',
+        install_requires=['requests','urllib3', 'certifi', 'cryptography', 'ujson'],
+        keywords='binance exchange rest api bitcoin ethereum btc eth neo',
+        classifiers=[
+                    'Intended Audience :: Developers',
+                    'License :: OSI Approved :: MIT License',
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python :: 3',
+                    'Programming Language :: Python :: 3.5',
+                    'Programming Language :: Python :: 3.6',
+                    'Programming Language :: Python :: 3.7',
+                    'Programming Language :: Python',
+                    'Topic :: Software Development :: Libraries :: Python Modules',
+                ],
+        python_requires='>=3.0',
+    )
