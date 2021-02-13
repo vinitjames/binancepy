70e55ac49c1d088d6ebb79d711efa7a5e8532fbe
vinit <vinit.james24@gmail.com>
2021-02-13 18:47:14 +0100

    modified code structure to implement endpoints as meta classes

---
+++ binance/endpoints/market_data.py
@@ -0,0 +1,140 @@
+from abc import ABCMeta, abstractmethod
+from typing import Union
+from binance.utils import format_time, interval_to_ms
+
+class MarketDataEndpoints(metaclass = ABCMeta):
+
+    @property
+    @abstractmethod
+    def request_handler(self):
+        pass
+
+    @abstractmethod
+    def _create_api_uri(self, path: str, version:str) -> str:
+        pass
+        
+
+    def ping(self) -> dict:
+        uri = self._create_api_uri('ping')
+        return self.request_handler.get(uri)
+
+    def get_server_time(self) -> dict:
+        uri = self._create_api_uri('time')
+        return self.request_handler.get(uri)
+
+    def get_exchange_info(self) -> dict:
+        uri = self._create_api_uri('exchangeInfo')
+        return self.request_handler.get(uri)
+
+    def get_symbol_info(self, symbol: str) -> dict:
+        resp_data = self.get_exchange_info()
+        for sym_data in resp_data['symbols']:
+            if(sym_data['symbol'] == symbol.upper()):
+                return sym_data
+        return None
+
+    def get_price_ticker(self, symbol: str = None) -> dict:
+        uri = self._create_api_uri('ticker/price')
+        if(symbol == None):
+            return self.request_handler.get(uri)
+        return self.request_handler.get(uri, symbol=symbol)
+
+    def get_orderbook_ticker(self, symbol: str = None) -> dict:
+        uri = self._create_api_uri('ticker/bookTicker')
+        if(symbol == None):
+            return self.request_handler.get(uri)
+        return self.request_handler.get(uri, symbol=symbol)
+
+    def get_order_book(self, symbol: str, limit: int = 100):
+        uri = self._create_api_uri('depth')
+        return self.request_handler.get(uri, symbol=symbol, limit=limit)
+
+    def get_avg_price(self, symbol: str) -> dict:
+        #avg price does not work with v1
+        uri = self._create_api_uri('avgPrice', version='v3')
+        return self.request_handler.get(uri, symbol=symbol)
+
+    def get_24hr_ticker(self, symbol: str = None) -> dict:
+        uri = self._create_api_uri('ticker/24hr')
+        if(symbol == None):
+            return self.request_handler.get(uri)
+        return self.request_handler.get(uri, symbol=symbol)
+    
+    def get_recent_trades(self, symbol: str, limit: int = 100) -> dict:
+        uri = self._create_api_uri('trades')
+        return self.request_handler.get(uri, symbol=symbol, limit=limit)
+
+    def get_agg_trades(self, symbol: str,
+                       formId: int = None,
+                       startTime: int = None,
+                       endTime: int = None,
+                       limit: int = 500):
+        params = locals()
+        del params['self']
+        if(params['startTime'] != None):
+            params['startTime'] = format_time(params['startTime'])
+        if(params['endTime'] != None):
+            params['endTime'] = format_time(params['endTime'])
+        params = {k:v for k,v in params.items() if v is not None}
+        uri = self._create_api_uri('aggTrades')
+        return self.request_handler.get(uri, **params)
+
+    def get_klines(self,
+                   symbol: str,
+                   interval: str,
+                   startTime: Union[int, str] = None,
+                   endTime: Union[int, str] = None,
+                   limit: int = None) -> dict:
+        
+        params = locals()
+        del params['self']
+        if(params['startTime'] != None):
+            params['startTime'] = format_time(params['startTime'])
+        if(params['endTime'] != None):
+            params['endTime'] = format_time(params['endTime'])
+        params = {k:v for k,v in params.items() if v is not None}
+        uri = self._create_api_uri('klines')
+        return self.request_handler.get(uri, **params)
+
+    def _get_earliest_valid_timestamp(self, symbol:str, interval:str):
+                
+        kline = self.get_klines(
+            symbol=symbol,
+            interval=interval,
+            limit=1,
+            startTime=0,
+            endTime=None)
+        return kline[0][0]
+
+    def get_historical_klines(self,
+                              symbol: str,
+                              interval: str,
+                              startTime: Union[int, str],
+                              endTime: Union[int, str] = None) -> dict:
+        params = locals()
+        del params['self']
+        earliest_timestamp = self._get_earliest_valid_timestamp(symbol,
+                                                                interval)
+        
+        params['startTime'] = format_time(params['startTime'])
+        params['startTime'] = max(earliest_timestamp, params[
+            'startTime'])
+        if(params['endTime'] != None):
+            params['endTime'] = format_time(params['endTime'])
+        if(params['endTime'] != None) and(params['startTime'] > params['endTime']):
+            raise ValueError('startTime entered is greater than endTime')
+        params = {k:v for k,v in params.items() if v is not None}
+        params['limit'] = 500
+        data = []
+        api_call_count = 0
+        while(True):
+            fetched_data = self.get_klines(**params)
+            api_call_count+=1
+            data.extend(fetched_data)
+            if(len(fetched_data) < params['limit']):
+                break
+            params['startTime'] = fetched_data[-1][0] + interval_to_ms(interval)
+            if (api_call_count) == 3:
+                time.sleep(0.5)
+                api_call_count = 0
+        return data
--- binance/public_client.py
+++ binance/public_client.py
@@ -1,14 +1,13 @@
 
 from .api_def import PublicAPI
 from .exceptions import BinanceAPIException, BinanceRequestException
-from .utils import format_time, interval_to_ms
-#from requests import Session
-#from requests.models import Response
+
+from binance.endpoints.market_data import MarketDataEndpoints
 from .request_handler import RequestHandler
 from typing import Union
 import time
 
-class PublicClient(PublicAPI):
+class PublicClient(PublicAPI, MarketDataEndpoints):
 
     def __init__(self,
                  endpoint_version: str = '',
@@ -16,171 +15,15 @@ class PublicClient(PublicAPI):
                  tld: str = 'com'):
         
         self.API_URL = self.API_URL.format(endpoint_version, tld)
-        #self.session = self._init_session()
-        #self.request_params = request_params
-        self.request_handler = RequestHandler(request_params = request_params)
+        self._request_handler = RequestHandler(request_params = request_params)
 
-    '''
-        def _init_session(self) -> Session:
-        session = Session()
-        session.headers.update({'Accept': 'application/json',
-                                'User-Agent': 'binance/python'})
-        return session
-    '''
 
+    @property
+    def request_handler(self):
+        return self._request_handler
+    
     def _create_api_uri(self, path: str, version=PublicAPI.PUBLIC_API_VERSION) -> str:
         return self.API_URL + '/' + version + '/' + path
-    '''
-    def _request_public(self, uri, **params):
-
-        kwargs = {}
-        kwargs['params'] = params
-        kwargs['timeout'] = 10
-        if self.request_params:
-            kwargs.update(self.request_params)
-        response = self.session.get(uri, **kwargs)
-        return self._handle_response(response)
- 
-    @classmethod
-    def _handle_response(cls, response: Response) -> dict:
-        if(type(response) != Response):
-            raise ValueError(
-                "Client Error: _handle resource was not called with Response Type")
-        if(not str(response.status_code).startswith('2')):
-            raise BinanceAPIException(response)
-
-        try:
-            return response.json()
-        except ValueError:
-            raise BinanceRequestException(response)
-    '''
-    
-    def ping(self) -> dict:
-        uri = self._create_api_uri('ping')
-        return self.request_handler.get(uri)
-
-    def get_server_time(self) -> dict:
-        uri = self._create_api_uri('time')
-        return self.request_handler.get(uri)
-
-    def get_exchange_info(self) -> dict:
-        uri = self._create_api_uri('exchangeInfo')
-        return self.request_handler.get(uri)
-
-    def get_symbol_info(self, symbol: str) -> dict:
-        resp_data = self.get_exchange_info()
-        for sym_data in resp_data['symbols']:
-            if(sym_data['symbol'] == symbol.upper()):
-                return sym_data
-        return None
-
-    def get_price_ticker(self, symbol: str = None) -> dict:
-        uri = self._create_api_uri('ticker/price')
-        if(symbol == None):
-            return self.request_handler.get(uri)
-        return self.request_handler.get(uri, symbol=symbol)
-
-    def get_orderbook_ticker(self, symbol: str = None) -> dict:
-        uri = self._create_api_uri('ticker/bookTicker')
-        if(symbol == None):
-            return self.request_handler.get(uri)
-        return self.request_handler.get(uri, symbol=symbol)
-
-    def get_order_book(self, symbol: str, limit: int = 100):
-        uri = self._create_api_uri('depth')
-        return self.request_handler.get(uri, symbol=symbol, limit=limit)
-
-    def get_avg_price(self, symbol: str) -> dict:
-        #avg price does not work with v1
-        uri = self._create_api_uri('avgPrice', version='v3')
-        return self.request_handler.get(uri, symbol=symbol)
-
-    def get_24hr_ticker(self, symbol: str = None) -> dict:
-        uri = self._create_api_uri('ticker/24hr')
-        if(symbol == None):
-            return self.request_handler.get(uri)
-        return self.request_handler.get(uri, symbol=symbol)
-    
-    def get_recent_trades(self, symbol: str, limit: int = 100) -> dict:
-        uri = self._create_api_uri('trades')
-        return self.request_handler.get(uri, symbol=symbol, limit=limit)
-
-    def get_agg_trades(self, symbol: str,
-                       formId: int = None,
-                       startTime: int = None,
-                       endTime: int = None,
-                       limit: int = 500):
-        params = locals()
-        del params['self']
-        if(params['startTime'] != None):
-            params['startTime'] = format_time(params['startTime'])
-        if(params['endTime'] != None):
-            params['endTime'] = format_time(params['endTime'])
-        params = {k:v for k,v in params.items() if v is not None}
-        uri = self._create_api_uri('aggTrades')
-        return self.request_handler.get(uri, **params)
-
-    def get_klines(self,
-                   symbol: str,
-                   interval: str,
-                   startTime: Union[int, str] = None,
-                   endTime: Union[int, str] = None,
-                   limit: int = None) -> dict:
-        
-        params = locals()
-        del params['self']
-        if(params['startTime'] != None):
-            params['startTime'] = format_time(params['startTime'])
-        if(params['endTime'] != None):
-            params['endTime'] = format_time(params['endTime'])
-        params = {k:v for k,v in params.items() if v is not None}
-        uri = self._create_api_uri('klines')
-        return self.request_handler.get(uri, **params)
-
-    def _get_earliest_valid_timestamp(self, symbol:str, interval:str):
-                
-        kline = self.get_klines(
-            symbol=symbol,
-            interval=interval,
-            limit=1,
-            startTime=0,
-            endTime=None)
-        return kline[0][0]
-
-    def get_historical_klines(self,
-                              symbol: str,
-                              interval: str,
-                              startTime: Union[int, str],
-                              endTime: Union[int, str] = None) -> dict:
-        params = locals()
-        del params['self']
-        earliest_timestamp = self._get_earliest_valid_timestamp(symbol,
-                                                                interval)
-        
-        params['startTime'] = format_time(params['startTime'])
-        params['startTime'] = max(earliest_timestamp, params[
-            'startTime'])
-        if(params['endTime'] != None):
-            params['endTime'] = format_time(params['endTime'])
-        if(params['endTime'] != None) and(params['startTime'] > params['endTime']):
-            raise ValueError('startTime entered is greater than endTime')
-        params = {k:v for k,v in params.items() if v is not None}
-        params['limit'] = 500
-        data = []
-        api_call_count = 0
-        while(True):
-            fetched_data = self.get_klines(**params)
-            api_call_count+=1
-            data.extend(fetched_data)
-            if(len(fetched_data) < params['limit']):
-                break
-            params['startTime'] = fetched_data[-1][0] + interval_to_ms(interval)
-            if (api_call_count) == 3:
-                time.sleep(0.5)
-                api_call_count = 0
-        return data
-            
-            
 
 if __name__ == '__main__':
     pass
